32caa29266e48809c59d6de4538ec8209d3e9c2c
aronyanez <30335407+aronyanez@users.noreply.github.com>
2018-06-28 19:14:12 -0500

    Init Repositoty

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Whatsapp-Contact-Prestashop
+# A PRODUCT ALERT
+
+This proyect put a whats contact
+
+## Getting Started
+
+You only need a site with prestashop and you should install a module .zip
+
+
+### Prerequisites
+
+-PRESTASHOP 1.7.x or higher version
+
+
+
+## Built With
+
+* [PrestaShop DevDocs](https://devdocs.prestashop.com/) - CMS used
+* [Animate.css](https://daneden.github.io/animate.css/) - CSS animations
+
+
+## Authors
+
+* **Arón Yáñez** - *Developer* - [Markdevs](https://markdevs.com)
+
+
+## License
+
+This project is licensed under the  GNU GENERAL PUBLIC LICENSE - see the [LICENSE.md](LICENSE.md) file for details
---
+++ translations/es.php
@@ -0,0 +1,18 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_bfa0c4d0cfa3ffdb63bc11dcbb4ac3d9'] = 'Nueva oferta';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_6f25a5b96bab2d94f54bec0fb3ab5b8f'] = 'Agrega una alerta con un producto popular';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_fe5d926454b6a8144efce13a44d019ba'] = 'Configuración NO válida';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_c888438d14855d7d96a2724ee9c306bd'] = 'Ajustes actualizados';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_f4f70727dc34561dfde1a3c529b6205c'] = 'Ajustes';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_a9ded1e5ce5d75814730bb4caaf49419'] = 'Fondo';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_91fdce8ad8c233c6f3eeb18e30dfc3b7'] = 'Ingresar código de color';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_ad32ba59a021a0d085e49b3659b49a61'] = 'Color de fuente';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_fa20e88573ea59fba619fb728edb1bea'] = 'Animación';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_10c540709bbfdbefb634dc3955427f25'] = 'Selecciona una animación';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_47dc103b7530febe3d1eca808a63059c'] = 'ID del producto';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_e4a1df22823901f99194c011cd612b4e'] = 'ingresa identificador';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_c9cc8cce247e49bae79f15173ce97354'] = 'Guardar';
+$_MODULE['<{mark_newoffer}prestashop>mark_newoffer_630f6dc397fe74e52d5189e2c80f282b'] = 'Regresar los módulos';
