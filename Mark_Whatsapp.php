d5cf43b695c11c19efcf83ca502effb7c898876b
Unknown <aronyanez@outlook.com>
2018-06-30 02:11:51 -0500

    v5

--- Mark_Whatsapp.php
+++ Mark_Whatsapp.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
 * 2007-2018 PrestaShop
 *
@@ -25,164 +26,177 @@
 */
 
 if (!defined('_PS_VERSION_')) {
-	exit;
+    exit;
 }
+
+
 class Mark_Whatsapp extends Module
 {
 
-	public function __construct()
-	{
-		$this->name = 'mark_whatsapp';
-		$this->author = 'Arón Yáñez';
-		$this->version = '1.0.0';
-		$this->tab = 'social_networks';
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-		$this->bootstrap = true;
-		parent::__construct();
-		$this->displayName = $this->l('Whatsapp Contact');
-		$this->description = $this->l('Add Whatsapp Contact');
-		$this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
-	}
-	public function install()
-	{
+    public function __construct()
+    {
+        $this->name = 'mark_whatsapp';
+        $this->author = 'Arón Yáñez';
+        $this->version = '1.0.0';
+        $this->tab = 'social_networks';
+        $this->ps_versions_compliancy = array(
+            'min' => '1.6',
+            'max' => _PS_VERSION_
+        );
+        $this->bootstrap = true;
+        parent::__construct();
 
-		if (Shop::isFeatureActive()) 
-		{
-			Shop::setContext(Shop::CONTEXT_ALL);
-		}
+        $this->displayName = $this->l('Whatsapp Contact');
+        $this->description = $this->l('Add Whatsapp Contact');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+    }
 
-		if (!parent::install() ||
-			!$this->registerHook('displayHeader') ||
-			!$this->registerHook('displayHome') ||
-			!Configuration::updateValue('Whats_Number', '4434395115') ||
-			!Configuration::updateValue('Whats_Message', $this->l('I want information'))) 
-		{
-			return false;
-		}
-		return true;
-	}
-	public function uninstall()
-	{
-		return parent::uninstall()
-		&& Configuration::deleteByName('Whats_Number')
-		&& Configuration::deleteByName('Whats_Message');
-	}
-	public function hookDisplayHeader($params)
-	{
+    public function install()
+    {
+
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+        }
+
+        if (!parent::install() ||
+            !$this->registerHook('displayHeader') ||
+            !$this->registerHook('displayHome') ||
+            !Configuration::updateValue('Whats_Number', '4434395115') ||
+            !Configuration::updateValue('Whats_Message', $this->l('I want information') )) 
+        {
+            return false;
+        }
+        return true;
+    }
 
 
-		$this->context->controller->registerStylesheet('whatsapp-icon', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css', array('server' => 'remote', 'position' => 'head','media' => 'all', 'priority' => 162));
+    public function uninstall()
+    {
+        return parent::uninstall()
+        && Configuration::deleteByName('Whats_Number')
+        && Configuration::deleteByName('Whats_Message');
+    }
 
-		$this->context->controller->registerStylesheet('whatsapp-style', 'modules/'.$this->name.'/views/css/style.css',array('media' => 'all', 'priority' => 163));
-	}
 
-	public function hookDisplayHome()
-	{
+    public function hookDisplayHeader($params)
+    {
 
-		$this ->context->smarty-> assign(array(
-			'Whats_Number' => Configuration::get('Whats_Number'),
-			'Whats_Message' => Configuration::get('Whats_Message')
-		));
 
-		return $this->display(__FILE__, 'views/templates/hook/Whatsapphook.tpl');
-	}
-	public function getContent()
-	{
-		$output = null;
+        $this->context->controller->registerStylesheet('modules-mark_whatsapp-icon', 'https://use.fontawesome.com/releases/v5.0.13/css/all.css', array('server' => 'remote', 'position' => 'head','media' => 'all', 'priority' => 162));
 
-		if (Tools::isSubmit('submit'.$this->name))
-		{
-			$Whats_Number= (string)Tools::getValue('Whats_Number');
-			$Whats_Message= (string)Tools::getValue('Whats_Message');
+        $this->context->controller->registerStylesheet('modules-whatsapp-style', 'modules/'.$this->name.'/views/css/style.css', 
+            array('media' => 'all', 'priority' => 163));
+    }
 
-			if ((!$Whats_Number 
-				|| empty($Whats_Number)
-				|| !Validate::isPhoneNumber($Whats_Number))
-				&& (!$Whats_Message || empty($Whats_Message)
-				|| !Validate::isString($Whats_Message)))
-				$output .= $this->displayError($this->l('Invalid Configuration value'));
+    public function hookDisplayHome()
+    {
 
-			else
-			{
-				Configuration::updateValue('Whats_Number', $Whats_Number);
-				Configuration::updateValue('Whats_Message', $Whats_Message);
-				$output .= $this->displayConfirmation($this->l('Settings updated'));
-			}
+        $this ->context->smarty-> assign(array(
+            'Whats_Number' => Configuration::get('Whats_Number'),
+            'Whats_Message' => Configuration::get('Whats_Message')
+        ));
+        //
 
-		}
+        return $this->display(__FILE__, 'views/templates/hook/Whatsapphook.tpl');
+    }
 
-		return $output.$this->displayForm();
-	}
-	public function displayForm()
-	{
-        // Get default language
-		$defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
-		$fieldsForm=array();
+    public function getContent()
+    {
+       $output = null;
 
-    	// Init Fields form array
-		$fieldsForm[0]['form'] = array(
-			'legend' => array(
-				'title' => $this->l('Settings'),
-			),
-			'input' => array(
-				array(
-					'type' => 'text',
-					'label' => $this->l('Phone Number'),
-					'desc' => $this->l('Your phone number'),
-					'name' => 'Whats_Number',
-					'size' => 10,
-					'required' => true,
-				),
-				array(
-					'type' => 'textarea',
-					'label' => $this->l('Message'),
-					'desc' => $this->l('Your initial Message'),
-					'name' => 'Whats_Message',
-					'size' => 100,
-					'required' => true,
-				)
-			),
+       if (Tools::isSubmit('submit'.$this->name))
+       {
+            $Whats_Number= (string)Tools::getValue('Whats_Number');
+            $Whats_Message= (string)Tools::getValue('Whats_Message');
 
-			'submit' => array(
-				'title' => $this->l('Save'),
-				'class' => 'btn btn-default pull-right'
-			)
-		);
+            if ( (!$Whats_Number || empty($Whats_Number) || !Validate::isPhoneNumber($Whats_Number))
+               &&   (!$Whats_Message || empty($Whats_Message)  || !Validate::isString($Whats_Message)) )
+                $output .= $this->displayError($this->l('Invalid Configuration value'));
 
-		$helper = new HelperForm();
+            else
+            {
+                Configuration::updateValue('Whats_Number', $Whats_Number);
+                Configuration::updateValue('Whats_Message', $Whats_Message);
+                $output .= $this->displayConfirmation($this->l('Settings updated'));
+            }
+
+        }
+    return $output.$this->displayForm();
+    }
+
+
+    public function displayForm()
+    {
+            // Get default language
+        $defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+        $fieldsForm=array();
+
+            // Init Fields form array
+        $fieldsForm[0]['form'] = array(
+            'legend' => array(
+                'title' => $this->l('Settings'),
+            ),
+            'input' =>  array(
+                array(
+                    'type' => 'text',
+                    'label' => $this->l('Phone Number'),
+                    'desc' => $this->l('Your phone number'),
+                    'name' => 'Whats_Number',
+                    'size' => 10,
+                    'required' => true,
+                ),
+                array(
+                    'type' => 'textarea',
+                    'label' => $this->l('Message'),
+                    'desc' => $this->l('Your initial Message'),
+                    'name' => 'Whats_Message',
+                    'size' => 100,
+                    'required' => true,
+                )
+            ),
+
+            'submit' => array(
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
+            )
+        );
+
+        $helper = new HelperForm();
 
         // Module, Token and currentIndex
-		$helper->module = $this;
-		$helper->name_controller = $this->name;
-		$helper->token = Tools::getAdminTokenLite('AdminModules');
-		$helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
 
         // Language
-		$helper->default_form_language = $defaultLang;
-		$helper->allow_employee_form_lang = $defaultLang;
+        $helper->default_form_language = $defaultLang;
+        $helper->allow_employee_form_lang = $defaultLang;
 
 
         // title and Toolbar
-		$helper->title = $this->displayName;
+        $helper->title = $this->displayName;
         $helper->show_toolbar = true;        // false -> remove toolbar
         $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
         $helper->submit_action = 'submit'.$this->name;
         $helper->toolbar_btn = array(
-        	'save' => array(
-        		'desc' => $this->l('Save'),
-        		'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
-        		'&token='.Tools::getAdminTokenLite('AdminModules'),
-        	), 
-        	'back' => array(
-        		'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
-        		'desc' => $this->l('Back to list')
-        	)
+            'save' => array(
+                'desc' => $this->l('Save'),
+                'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+                '&token='.Tools::getAdminTokenLite('AdminModules'),
+            ), 
+            'back' => array(
+                'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->l('Back to list')
+            )
         );
 
-        // Load current value
+            // Load current value
         $helper->fields_value['Whats_Number'] = Configuration::get('Whats_Number');
         $helper->fields_value['Whats_Message'] = Configuration::get('Whats_Message');
 
         return $helper->generateForm($fieldsForm);
     }
+
+
 }
