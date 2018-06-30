bd18ef96804b1c292ff3db787a4ea5b3ee1f6bf8
Unknown <aronyanez@outlook.com>
2018-06-30 01:59:38 -0500

    v4

--- Mark_Whatsapp.php
+++ Mark_Whatsapp.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
 * 2007-2018 PrestaShop
 *
@@ -24,12 +23,9 @@
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
 if (!defined('_PS_VERSION_')) {
 	exit;
 }
-
-
 class Mark_Whatsapp extends Module
 {
 
@@ -39,22 +35,18 @@ class Mark_Whatsapp extends Module
 		$this->author = 'Arón Yáñez';
 		$this->version = '1.0.0';
 		$this->tab = 'social_networks';
-		$this->ps_versions_compliancy = array(
-			'min' => '1.6',
-			'max' => _PS_VERSION_
-		);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
 		$this->bootstrap = true;
 		parent::__construct();
-
 		$this->displayName = $this->l('Whatsapp Contact');
 		$this->description = $this->l('Add Whatsapp Contact');
 		$this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 	}
-
 	public function install()
 	{
 
-		if (Shop::isFeatureActive()) {
+		if (Shop::isFeatureActive()) 
+		{
 			Shop::setContext(Shop::CONTEXT_ALL);
 		}
 
@@ -68,16 +60,12 @@ class Mark_Whatsapp extends Module
 		}
 		return true;
 	}
-
-
 	public function uninstall()
 	{
 		return parent::uninstall()
 		&& Configuration::deleteByName('Whats_Number')
 		&& Configuration::deleteByName('Whats_Message');
 	}
-
-
 	public function hookDisplayHeader($params)
 	{
 
@@ -94,90 +82,87 @@ class Mark_Whatsapp extends Module
 			'Whats_Number' => Configuration::get('Whats_Number'),
 			'Whats_Message' => Configuration::get('Whats_Message')
 		));
-        //
 
 		return $this->display(__FILE__, 'views/templates/hook/Whatsapphook.tpl');
 	}
+	public function getContent()
+	{
+		$output = null;
 
-		public function getContent()
+		if (Tools::isSubmit('submit'.$this->name))
 		{
-			$output = null;
+			$Whats_Number= (string)Tools::getValue('Whats_Number');
+			$Whats_Message= (string)Tools::getValue('Whats_Message');
 
-			if (Tools::isSubmit('submit'.$this->name))
+			if ((!$Whats_Number 
+				|| empty($Whats_Number)
+				|| !Validate::isPhoneNumber($Whats_Number))
+				&& (!$Whats_Message || empty($Whats_Message)
+				|| !Validate::isString($Whats_Message)))
+				$output .= $this->displayError($this->l('Invalid Configuration value'));
+
+			else
 			{
-				$Whats_Number= (string)Tools::getValue('Whats_Number');
-				$Whats_Message= (string)Tools::getValue('Whats_Message');
-
-				if ((!$Whats_Number 
-					|| empty($Whats_Number)
-					|| !Validate::isPhoneNumber($Whats_Number))
-					&& (!$Whats_Message || empty($Whats_Message)
-					|| !Validate::isString($Whats_Message)))
-						$output .= $this->displayError($this->l('Invalid Configuration value'));
-
-				else
-				{
-						Configuration::updateValue('Whats_Number', $Whats_Number);
-						Configuration::updateValue('Whats_Message', $Whats_Message);
-						$output .= $this->displayConfirmation($this->l('Settings updated'));
-				}
-
+				Configuration::updateValue('Whats_Number', $Whats_Number);
+				Configuration::updateValue('Whats_Message', $Whats_Message);
+				$output .= $this->displayConfirmation($this->l('Settings updated'));
 			}
-				return $output.$this->displayForm();
+
 		}
 
+		return $output.$this->displayForm();
+	}
+	public function displayForm()
+	{
+        // Get default language
+		$defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+		$fieldsForm=array();
 
-		public function displayForm()
-		{
-            // Get default language
-			$defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
-			$fieldsForm=array();
-
-            // Init Fields form array
-			$fieldsForm[0]['form'] = array(
-				'legend' => array(
-					'title' => $this->l('Settings'),
+    	// Init Fields form array
+		$fieldsForm[0]['form'] = array(
+			'legend' => array(
+				'title' => $this->l('Settings'),
+			),
+			'input' => array(
+				array(
+					'type' => 'text',
+					'label' => $this->l('Phone Number'),
+					'desc' => $this->l('Your phone number'),
+					'name' => 'Whats_Number',
+					'size' => 10,
+					'required' => true,
 				),
-				'input' =>  array(
-					array(
-						'type' => 'text',
-						'label' => $this->l('Phone Number'),
-						'desc' => $this->l('Your phone number'),
-						'name' => 'Whats_Number',
-						'size' => 10,
-						'required' => true,
-					),
-					array(
-						'type' => 'textarea',
-						'label' => $this->l('Message'),
-						'desc' => $this->l('Your initial Message'),
-						'name' => 'Whats_Message',
-						'size' => 100,
-						'required' => true,
-					)
-				),
-
-				'submit' => array(
-					'title' => $this->l('Save'),
-					'class' => 'btn btn-default pull-right'
+				array(
+					'type' => 'textarea',
+					'label' => $this->l('Message'),
+					'desc' => $this->l('Your initial Message'),
+					'name' => 'Whats_Message',
+					'size' => 100,
+					'required' => true,
 				)
-			);
+			),
 
-			$helper = new HelperForm();
+			'submit' => array(
+				'title' => $this->l('Save'),
+				'class' => 'btn btn-default pull-right'
+			)
+		);
+
+		$helper = new HelperForm();
 
         // Module, Token and currentIndex
-			$helper->module = $this;
-			$helper->name_controller = $this->name;
-			$helper->token = Tools::getAdminTokenLite('AdminModules');
-			$helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+		$helper->module = $this;
+		$helper->name_controller = $this->name;
+		$helper->token = Tools::getAdminTokenLite('AdminModules');
+		$helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
 
         // Language
-			$helper->default_form_language = $defaultLang;
-			$helper->allow_employee_form_lang = $defaultLang;
+		$helper->default_form_language = $defaultLang;
+		$helper->allow_employee_form_lang = $defaultLang;
 
 
         // title and Toolbar
-			$helper->title = $this->displayName;
+		$helper->title = $this->displayName;
         $helper->show_toolbar = true;        // false -> remove toolbar
         $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
         $helper->submit_action = 'submit'.$this->name;
@@ -193,12 +178,10 @@ class Mark_Whatsapp extends Module
         	)
         );
 
-            // Load current value
+        // Load current value
         $helper->fields_value['Whats_Number'] = Configuration::get('Whats_Number');
         $helper->fields_value['Whats_Message'] = Configuration::get('Whats_Message');
 
         return $helper->generateForm($fieldsForm);
     }
-
-
 }
